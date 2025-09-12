1e76a97f18234879b131bf158985f472489bb619
dairuman <2419974912@qq.com>
2025-09-12 10:33:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 weight=input()
 if weight[-2:]=="kg":
   t=2.2046*eval(weight[0:2])
-  print(＂对应的英制重量为{:.3f}磅".format(t))
+  print("对应的英制重量为{:.3f}磅".format(t))
 elif weight[-2:]=="pd":
   m=eval(weight[0:2])/2.2046
   print("对应的公制重量为{:.3f}千克".format(m))
