af169ff7719313eec92d8b622318439c9f4fad58
dairuman <2419974912@qq.com>
2025-09-12 11:06:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # 在这个文件下编写代码，题目具体要求见README.md文件
-weight=input()
-if weight[-2:]=="kg":
-  t=2.2046*eval(weight[0:2])
-  print("对应的英制重量为{:.3f}磅".format(t))
-elif weight[-2:]=="pd":
-  m=eval(weight[0:2])/2.2046
-  print("对应的公制重量为{:.3f}千克".format(m))
+weight_str = input()
+unit = weight_str[-2:]
+value = float(weight_str[:-2])
+if unit == "kg":
+    pounds = value * 2.2046
+    print(f"对应的英制重量为{pounds:.3f}磅")
+elif unit == "pd":
+    kilograms = value * 0.4535
+    print(f"对应的公制重量为{kilograms:.3f}公斤")
+else:
+    print("输入格式错误")
