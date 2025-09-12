a8fd7a4294498e3ff21188d938930f9fa749ada2
dairuman <2419974912@qq.com>
2025-09-12 10:30:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # 在这个文件下编写代码，题目具体要求见README.md文件
 weight=input()
-if weight[-2:]==]"kg":
+if weight[-2:]=="kg":
   t=2.2046*eval(weight[0:2])
   print(＂对应的英制重量为{:.3f}磅".format(t))
 elif weight[-2:]=="pd":
